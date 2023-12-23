52e9c9efc2f2c5e6ead370c3afb75cc209641cc3
Phil <philippe.teigne@gmail.com>
2023-12-22 20:00:01 -0500

    implement forwarding formula and create testMessages variable

--- twitchChat/twitch.js
+++ twitchChat/twitch.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-style */
 const tmi = require('tmi.js');
 const { oauthToken } = require('../twitchAPICredentials');
 
@@ -11,40 +12,50 @@ const opts = {
   ]
 };
 
+const testMessages = [
+  {
+    username: 'test01',
+    timestamp: 'Dec-01',
+    message: 'This is just a test',
+    messageID: '123456'
+  },
+  {
+    username: 'test02',
+    timestamp: 'Dec-01',
+    message: 'This is just a test',
+    messageID: '123456'
+  },
+  {
+    username: 'test03',
+    timestamp: 'Dec-01',
+    message: 'This is just a test',
+    messageID: '123456'
+  }
+];
+
 // Create a client with our options
 const client = new tmi.client(opts);
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
+// client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
-client.connect().catch(console.error);
-
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
-}
-
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
+// When sent an array of youtube messages, format the messages as 'username: message', write the messages to twitch
+const forwardYTMessages = (target, messagesArray) => {
+  messagesArray.forEach(message => {
+    client.say(target, `${message.username}: ${message.message}`);
+  });
+};
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
+
+// Connect to Twitch:
+client.connect().catch(console.error)
+  .then(() => {
+    setTimeout(() => {
+      
+    }, 4000);
+  });
