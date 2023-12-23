b8a49a91bb7423206e96a1287ebd50b4276fad6c
Phil <philippe.teigne@gmail.com>
2023-12-23 13:20:26 -0500

    move socket to index.js, replace global references with arguments

--- twitchChat/twitch.js
+++ twitchChat/twitch.js
@@ -1,98 +1,12 @@
-/* eslint-disable func-style */
-const tmi = require('tmi.js');
-const { oauthToken } = require('../twitchAPICredentials');
-
-const opts = {
-  identity: {
-    username: 'yttbot',
-    password: oauthToken
-  },
-  channels: [
-    'ledfalcon'
-  ]
-};
-
-// Create a client with our options
-const client = new tmi.client(opts);
-
-// Register our event handlers (defined below)
-// client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
-// When sent an array of youtube messages, format the messages as 'username: message', write the messages to twitch
-const forwardYTMessages = (target) => {
-  const testMessages = [
-    {
-      username: 'test01',
-      timestamp: 'Dec-01',
-      message: 'This is just a test',
-      messageID: '123456'
-    },
-    {
-      username: 'test02',
-      timestamp: 'Dec-01',
-      message: 'This is just a test',
-      messageID: '123456'
-    },
-    {
-      username: 'test03',
-      timestamp: 'Dec-01',
-      message: 'This is just a test',
-      messageID: '123456'
-    }
-  ];
-
-  testMessages.forEach(message => {
-    client.say(target, `${message.username}: ${message.message}`);
-  });
-};
-
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-  // setTimeout((target) => {
-  //   const testMessages = [
-  //     {
-  //       username: 'test01',
-  //       timestamp: 'Dec-01',
-  //       message: 'This is just a test',
-  //       messageID: '123456'
-  //     },
-  //     {
-  //       username: 'test02',
-  //       timestamp: 'Dec-01',
-  //       message: 'This is just a test',
-  //       messageID: '123456'
-  //     },
-  //     {
-  //       username: 'test03',
-  //       timestamp: 'Dec-01',
-  //       message: 'This is just a test',
-  //       messageID: '123456'
-  //     }
-  //   ];
-  
-  //   testMessages.forEach(message => {
-  //     console.log('attempting to speak');
-  //     client.say(target, `${message.username}: ${message.message}`);
-  //   });
-  // }, 4000);
-
-}
-
-
-client.on('message', onMessageHandler);
-
-
 // Loop needs to wait for previous setTimeout to end before starting the next one
-const sayMessage = (target, arrayOfMessages) => {
-  if (arrayOfMessages.length > 1) {
+const sayMessage = (target, arrayOfMessages, webSocket) => {
+  if (arrayOfMessages.length > 0) {
 
     let i = 0;
 
     let slowMessages = setInterval(() => {
 
-      client.say(target, `${arrayOfMessages[i].username}: ${arrayOfMessages[i].message}`);
+      webSocket.say(target, `${arrayOfMessages[i].username}: ${arrayOfMessages[i].message}`);
       i++;
 
       // Break setInterval when all messages have been sent
@@ -100,38 +14,8 @@ const sayMessage = (target, arrayOfMessages) => {
         clearInterval(slowMessages);
       }
 
-    }, 2000);
+    }, 1501); // Found minimum 1025
   }
 };
 
-
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  const testMessages = [
-    {
-      username: 'test01',
-      timestamp: 'Dec-01',
-      message: 'This is just a test',
-      messageID: '123456'
-    },
-    {
-      username: 'test02',
-      timestamp: 'Dec-01',
-      message: 'This is just a test',
-      messageID: '123456'
-    },
-    {
-      username: 'test03',
-      timestamp: 'Dec-01',
-      message: 'This is just a test',
-      messageID: '123456'
-    }
-  ];
-
-  sayMessage(target, testMessages);
-}
-
-
-// Connect to Twitch:
-client.connect().catch(console.error);
+module.exports = { sayMessage };
