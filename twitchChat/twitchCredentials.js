bcb2c918bdfc382d965fe55153c25b2f276b9983
Phil <philippe.teigne@gmail.com>
2023-12-24 14:37:16 -0500

    reorganize code into more logical structure

---
+++ twitchChat/twitchCredentials.js
@@ -0,0 +1,18 @@
+const { oauthToken } = require('../twitchAPICredentials');
+const tmi = require('tmi.js');
+
+const opts = {
+  identity: {
+    username: 'yttbot',
+    password: oauthToken
+  },
+  channels: [
+    'ledfalcon'
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+// const client = new tmi.client(opts);
+
+module.exports = { client, opts };
