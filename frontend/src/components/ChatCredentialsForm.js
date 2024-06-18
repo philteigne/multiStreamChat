42582bd12513664a7d93a1b32dea6107a5525ea9
philteigne <philippe.teigne@gmail.com>
2024-06-17 22:35:27 -0400

    Create plceholder components

---
+++ frontend/src/components/ChatCredentialsForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatCredentialsForm = () => {
+  return(
+    <React.Fragment>
+      ChatCredentialsForm
+    </React.Fragment>
+  );
+}
+
+export default ChatCredentialsForm;
---
+++ frontend/src/components/ChatDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatDisplay = () => {
+  return(
+    <React.Fragment>
+      ChatDisplay
+    </React.Fragment>
+  );
+}
+
+export default ChatDisplay;
