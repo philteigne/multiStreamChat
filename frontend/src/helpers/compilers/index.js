c8dad580adf38a66d5e82bd72d6074300990e942
philteigne <philippe.teigne@gmail.com>
2024-06-23 19:47:36 -0400

    move youtube functions to youtube helpers as they no longer rely on twitch functions

--- frontend/src/helpers/compilers/index.js
+++ frontend/src/helpers/compilers/index.js
@@ -7,62 +7,17 @@ const fetch = require('node-fetch');
 
 // YOUTUBE
 const { fetchLiveChatURL } = require('../youtubeChat/youtubeCredentials.js');
-const { findNewMessages } = require('../youtubeChat/youtubeHelperFunctions.js');
+const { listenYoutube } = require('../youtubeChat/youtubeHelperFunctions.js');
+const { listenTwitch } = require('../twitchChat/twitchHelperFunctions.js');
 
-let lastChatID = "";
+const listenLive = (youtubeChannelID, googleAPIKey, totalComments, interval, callback) => {
 
-const forwardYTChat = (liveChatID, totalComments, googleAPIKey, interval) => {
-  let googleApiChat = `https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${liveChatID}&part=snippet,authorDetails&maxResults=${totalComments}&key=${googleAPIKey}`;
-  
-  console.log("Polling for new messages...\n");
+  // Listen to Twitch
+  listenTwitch('ledfalcon', callback)
 
-  //  continuously fetch youtube messages and check for new ones
-  setInterval(() => {
-    return fetch(googleApiChat)
-      .then(response => response.json())
-      .then((responseJSON) => {
-
-        // reverse message list order so the newest message is the first element
-        const messageArray = responseJSON.items.reverse();
-        const mostRecentMessage = messageArray[0].id;
-        
-        //  return to prevent forwarding all historical messages
-        if (lastChatID === '') {
-          lastChatID = mostRecentMessage;
-          return;
-        }
-        
-        const chatMessages = findNewMessages(messageArray, lastChatID);
-
-        chatMessages.forEach(message => {
-          console.log(`${message.sender.displayName}: ${message.message}`)
-        });
-        
-        // sayMessage(opts.channels[0], chatMessages, client);
-
-        lastChatID = mostRecentMessage;
-
-        return;
-      });
-  }, interval * 1000);
-  
-
-};
-
-// Called every time the bot connects to Twitch chat
-const onConnectedHandler = (addr, port) => {
-  console.log(`* Connected to ${addr}:${port}`);
-};
-
-// client.on('connected', onConnectedHandler);
-
-// Connect to Twitch:
-// client.connect().catch(console.error);
-
-
-const pollMessages = (youtubeChannelID, googleAPIKey, totalComments, interval) => {
+  // Listen to Youtube
   return fetchLiveChatURL(youtubeChannelID, googleAPIKey)
-    .then(liveChatID => forwardYTChat(liveChatID, totalComments, googleAPIKey, interval));
+    .then(liveChatID => listenYoutube(liveChatID, totalComments, googleAPIKey, interval, callback));
 };
 
-module.exports = { pollMessages };
+module.exports = { listenLive };
