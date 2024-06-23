ecf82d3110c8598f13fbb94170758cea73ffbad3
philteigne <philippe.teigne@gmail.com>
2024-06-22 20:07:57 -0400

    create starting state variables, import helper functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
-import React from 'react';
+import {React, useState} from 'react';
 
 import ChatCredentialsForm from './components/ChatCredentialsForm';
 import ChatDisplay from './components/ChatDisplay';
+
+import { pollMessages } from './helpers/compilers';
+import { buildClient } from './helpers/twitchChat/twitchCredentials';
 
 import './App.css';
 
 function App() {
 
+  const [googleAPIKey, setGoogleAPIKey] = useState("");
+
   const state = {
     channelCredentials: {
-      youtubeChannelID: "UCKnodHJpZd8UbSvAufDd3_g",
+      youtubeChannelID: "UCrPseYLGpNygVi34QpGNqpA",
       twitchChannelID: "ledfalcon",
       googleAPIKey: "placeholder" 
     }
   }
+
   return (
     <div>
+      <form>
+        <input onChange={(e) => {
+          setGoogleAPIKey(e.target.value)
+          console.log(googleAPIKey)
+          }}></input>
+      </form>
+      <button onClick={() => {
+          console.log(googleAPIKey)
+          pollMessages("UCrPseYLGpNygVi34QpGNqpA", googleAPIKey, 5, 5)
+        }}>
+        Start Polling
+      </button>
       <ChatDisplay />
       <ChatCredentialsForm />
     </div>
