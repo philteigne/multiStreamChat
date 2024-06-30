eadec5ce6242874ed157dc273d7905197ac44279
philteigne <philippe.teigne@gmail.com>
2024-06-30 11:21:49 -0400

    update compiler to take twitch channel input from app

--- frontend/src/helpers/compilers/index.js
+++ frontend/src/helpers/compilers/index.js
@@ -10,16 +10,25 @@ const { fetchLiveChatURL } = require('../youtubeChat/youtubeCredentials.js');
 const { listenYoutube } = require('../youtubeChat/youtubeHelperFunctions.js');
 const { listenTwitch } = require('../twitchChat/twitchHelperFunctions.js');
 
-const listenLive = (youtubeChannelID, googleAPIKey, totalComments, interval, callback) => {
+const listenLive = (youtubeChannelID, twitchChannel, googleAPIKey, totalComments, interval, callback) => {
 
   // Listen to Twitch
-  const twitchClient = listenTwitch('ledfalcon', callback);
+  const twitchClient = listenTwitch(twitchChannel, callback);
+  const stopTwitchListening = () => {
+    twitchClient.disconnect()
+  }
+
+  // Check if twitchClient has a disconnect method
+  if (typeof twitchClient.disconnect !== 'function') {
+    throw new Error('Twitch client does not have a disconnect method');
+  } else {
+    console.log("got it")
+  }
 
   // Listen to Youtube
   return fetchLiveChatURL(youtubeChannelID, googleAPIKey)
     .then(liveChatID => {
       const stopYoutubeListening = listenYoutube(liveChatID, totalComments, googleAPIKey, interval, callback);
-      const stopTwitchListening = () => twitchClient.disconnect()
 
       return {
         stopYoutubeListening,
