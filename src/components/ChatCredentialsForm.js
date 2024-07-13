75e613beb5d0bd90a68c0c0a0dbbced29dab37b5
philteigne <philippe.teigne@gmail.com>
2024-07-13 17:34:14 -0400

    Set input values to display current state

--- src/components/ChatCredentialsForm.js
+++ src/components/ChatCredentialsForm.js
@@ -1,6 +1,6 @@
 import {React} from 'react';
 
-const ChatCredentialsForm = ({setYoutubeChannelID, setTwitchChannelName, setGoogleAPIKey, handleSubmit}) => {
+const ChatCredentialsForm = ({youtubeChannelID, setYoutubeChannelID, twitchChannelName, setTwitchChannelName, googleAPIKey, setGoogleAPIKey, handleSubmit}) => {
 
   return(
     <form>
@@ -8,19 +8,19 @@ const ChatCredentialsForm = ({setYoutubeChannelID, setTwitchChannelName, setGoog
         <label>
           YOUTUBE CHANNEL ID
         </label>
-        <input onChange={(e) => setYoutubeChannelID(e.target.value)}></input>
+        <input onChange={(e) => setYoutubeChannelID(e.target.value)} value={youtubeChannelID}></input>
       </span>
       <span className='form-input'>
         <label>
           TWITCH CHANNEL ID
         </label>
-        <input onChange={(e) => setTwitchChannelName(e.target.value)}></input>
+        <input onChange={(e) => setTwitchChannelName(e.target.value)} value={twitchChannelName}></input>
       </span>
       <span className='form-input'>
         <label>
           GOOGLE API KEY
         </label>
-        <input onChange={(e) => setGoogleAPIKey(e.target.value)} type="password"></input>
+        <input onChange={(e) => setGoogleAPIKey(e.target.value)} type="password" value={googleAPIKey}></input>
       </span>
       <button className='form-button' onClick={() => {
           handleSubmit()
